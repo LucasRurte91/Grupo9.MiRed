be1a73c40c33c9816f94aad5e88180ec79b2ff81
JuanBDM <88855781+JuanBDM@users.noreply.github.com>
2021-09-26 19:00:55 -0300

    Juan Cruz commits

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const { prototype } = require("events");
 const express = require("express");
 const app = express();
-const path = require ("path")
+const path = require("path");
 app.use(express.static("public"));
 
-
-app.listen (3006, () => {
-    console.log("servidor levantado en: http://localhost:3006/");
-})
-
-app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname,"./views/register.html"))
+app.listen(3030, ()=>{ 
+    console.log("El servidor RED corriendo en: http://localhost:3030/");
 });
 
-app.get("/productCart",  (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/productCart.html"));
-}); 
+app.get("/", (req, res)=> {
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+});
 
+app.get("/login", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+})
+
+app.get("/detalleDelProducto", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/detalleDelProducto.html"))
+})
 
