fa85587d9ca25abb5baf9a7e28f1d33991ba552b
LucasRurte91 <88855813+LucasRurte91@users.noreply.github.com>
2021-12-21 18:03:08 -0300

    Sprint 5

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ app.use(session({
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'))
 
-
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')));
